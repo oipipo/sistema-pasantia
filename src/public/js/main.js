94a09a8da19790ae14fad1e23082e2fdf4d91286
ricardo vasquez <rjvasquez1996@gmail.com>
2018-04-02 21:11:25 -0400

    updated

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,6 +2,6 @@ var pre="FALSE";
 var pos="FALSE";
 var datos={};
 var logo =new Image;
-logo.src="/images/uneg carta.png"
+logo.src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/LOGO_UNEG.jpg/240px-LOGO_UNEG.jpg"
 var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 var f=new Date();
